03f8cf696336bfb5a1e14155b248fd7e27cd49e5
Ramanpreet Singh <singh.ramanpreet09@gmail.com>
2019-11-03 11:55:03 +0100

    2018 changes, training

--- training/run_train_bdt.sh
+++ training/run_train_bdt.sh
@@ -2,11 +2,12 @@
 echo "Setting Env"
 source /cvmfs/sft.cern.ch/lcg/views/LCG_96python3/x86_64-centos7-gcc8-opt/setup.sh
 
-era="94X"
-max_N=500000
-jet_type="chs"
-in_dir="/afs/cern.ch/work/s/singhr/jets_study/PUJetId_training_plotting/training/input/"
+era=$1
+max_N=$2
+jet_type=$3
+in_dir=$4
 d_name="BDT_${jet_type}_${era}"
+eta_bin=$5
 
 mkdir -p output
 cd output
@@ -14,6 +15,6 @@ cd output
 mkdir -p $d_name
 
 chmod +x ../train_bdt.py
-../train_bdt.py --era $era --max_N $max_N --jet_type $jet_type --in_dir $in_dir --d_name $d_name --eta_bins $1
+../train_bdt.py --era $era --max_N $max_N --jet_type $jet_type --in_dir $in_dir --d_name $d_name --eta_bins $eta_bin
 
 cd ../
